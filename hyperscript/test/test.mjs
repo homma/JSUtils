0fb59e9b0a1362a87497e7942bdf9378f7371364
homma <daisuke.homma@gmail.com>
2018-09-02 02:14:12 +0900

    committed on 2018/09/02 02:14.

---
+++ hyperscript/test/test.mjs
@@ -0,0 +1,44 @@
+import * as h from "../hyperscript.mjs";
+
+const test1 = () => {
+  console.log("BEGIN TEST.");
+
+  const elem = h(
+    "div#win",
+    {
+      style: {
+        width: 100,
+        height: 200,
+        overflow: "auto"
+      }
+    },
+    h("div#logArea"),
+    h("span#prompt", " > ", {
+      style: {
+        color: "black",
+        "background-color": "blue"
+      }
+    }),
+    h("span#editArea", {
+      contentEditable: true,
+      style: { color: "black" }
+    }),
+    h("div#paddingArea")
+  );
+
+  document.body.appendChild(elem);
+  console.log(elem);
+
+  console.log("END TEST.");
+};
+
+const test2 = () => {
+  // To be implemented
+};
+
+const main = () => {
+  test1();
+  test2();
+};
+
+window.onload = main;
