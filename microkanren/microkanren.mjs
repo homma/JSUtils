eb4c2b1a5185ebb3364a295369ac4572afb3b527
homma <daisuke.homma@gmail.com>
2019-01-03 03:54:02 +0900

    committed on 2019/01/03 03:54.

--- microkanren/microkanren.mjs
+++ microkanren/microkanren.mjs
@@ -113,7 +113,7 @@ export //
 const get_fun = strm => {
   if (!is_fun(strm)) {
     console.log(
-      `ERROR: ${strm} is not a function in the last of an immature stream.`
+      `ERROR: ${strm} is not a function at the last of an immature stream.`
     );
   }
 
